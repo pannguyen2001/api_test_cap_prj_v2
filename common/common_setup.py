b02a9f26a8fff5c415ff06585ba92a18dea36827
truonghoc19102001 <truonghoc19102001@gmail.com>
2026-05-30 07:36:50 +0000

    update code

--- common/common_setup.py
+++ common/common_setup.py
@@ -22,3 +22,16 @@ logger.success(f"Load role predata successfully:\n{json.dumps(role_predata_info,
 account_request_body_template_file_path: str = os.path.join(base_dir, "data", "request_body_template", "account_request_body_template.json")
 account_predata_info: Dict = load_data(account_request_body_template_file_path, "json")
 logger.success(f"Load account predata successfully:\n{json.dumps(account_predata_info, indent=4)}")
+
+# =========== Load client info from USER_CONFIG_FILE_PATH ==========
+user_config_file_path: str = os.path.join(base_dir, "configs", "user_info.json")
+client_info: dict = load_data(user_config_file_path, "json")
+admin_info: dict = client_info["admin"]
+student_info: dict = client_info["student"]
+teacher_info: dict = client_info["teacher"]
+logger.success(f"Load client info successfully:\n{json.dumps(client_info, indent=4)}")
+
+# ========== User login ==========
+# admin: User = User(ROLE.ADMIN.value, **admin_info)
+# student: User = User(ROLE.STUDENT.value, **student_info)
+# teacher: User = User(ROLE.TEACHER.value, **teacher_info)
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     pytest.main([
         test_case_path,
         "-m",
-        "account",
+        mark,
         # "--clean-alluredir"
     ])
     logger.success(f"{' End testing ':=^50}\n")
